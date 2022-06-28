a6e3df40980a3768a56cbf3f473ae0735e21440f
Sebastian August Berg <sebasabe@stud.ntnu.no>
2022-06-28 15:49:04 +0200

    Update sql code for database setup

--- database/f1fantasy.sql
+++ database/f1fantasy.sql
@@ -4,7 +4,6 @@ CREATE TABLE drivers (
     initials VARCHAR(3) NOT NULL,
     price INT NOT NULL,
     points INT,
-    UNIQUE(initials),
     PRIMARY KEY(initials)
 );
 
@@ -12,7 +11,6 @@ CREATE TABLE constructors (
     abbreviation VARCHAR(3) NOT NULL,
     price INT NOT NULL,
     points INT,
-    UNIQUE(abbreviation),
     PRIMARY KEY(abbreviation)
 );
 
